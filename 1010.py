1355e168ed08611696ec2e82fe293c3425d6758d
Vinícius Sousa <vini.antunes.sousa@gmail.com>
2022-10-10 08:33:55 -0300

    Update 1010.py

--- 1010.py
+++ 1010.py
@@ -1,4 +1,4 @@
-import doctest
+inventario = {}
 
 def listar_inventario() ->dict[str,int]:
     """ retorna o inventario
@@ -6,7 +6,8 @@ def listar_inventario() ->dict[str,int]:
     Returns:
         dict[str,int]: inventário
     """    
-
+    global inventario
+    return inventario
 
 def cadastrar_item(item: str, quantidade: int)-> dict[str, int]:
     """Cadastra um item no inventário
@@ -18,7 +19,12 @@ def cadastrar_item(item: str, quantidade: int)-> dict[str, int]:
     Returns:
         dict[str, int]: inventário
     """    
-
+    global inventario
+    if item in inventario.keys():
+        inventario[item] += quantidade
+    else:
+        inventario[item] = quantidade
+    return inventario
 
 def excluir_item(item: str) -> dict[str, int]:
     """Exclui um item do inventário
@@ -29,7 +35,10 @@ def excluir_item(item: str) -> dict[str, int]:
     Returns:
         dict[str, int]: iventário atualizado
     """    
-
+    global inventario
+    if item in inventario:
+        inventario = inventario.pop(item)
+    return inventario
 
 def buscar_item(item: str) -> int|bool:
     """buscar um item no inventario
@@ -40,3 +49,7 @@ def buscar_item(item: str) -> int|bool:
     Returns:
         int|bool: quantidade do item. Se o item não existir retorna False
     """    
+    global inventario
+    if item in inventario:
+        return inventario[item]
+    return False
