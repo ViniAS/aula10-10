15934d4e32b62b9ea102218b1c0b10fac68683b0
LuisHDBueno <105614892+LuisHDBueno@users.noreply.github.com>
2022-10-10 09:11:46 -0300

    update teste.py

--- doct/teste.py
+++ doct/teste.py
@@ -24,8 +24,17 @@ class TestModuloQualidade(unittest.TestCase):
     def tearDown(self):
         print('\nExecutando tearDownMethod')
     def test_case_funcao_2(self):
-        print('Teste de alerta ponto de fulgor etanol verdadeiro')
-        self.assertTrue(mq.funcao2('bola',10,{"item1": 1, "item2": 3, "item3": 6}),{'item1': 1, 'item2': 3, 'item3': 6, 'bola': 10})
+        print('Teste Função 2')
+        self.assertEqual(mq.funcao2('bola',10,{"item1": 1, "item2": 3, "item3": 6}),{'item1': 1, 'item2': 3, 'item3': 6, 'bola': 10})
+    def test_case_funcao_2_item_existente(self):
+        print('Teste Função 2 item existente')
+        self.assertEqual(mq.funcao2('bola',10,{'item1': 1, 'item2': 3, 'item3': 6, 'bola': 10}),{'item1': 1, 'item2': 3, 'item3': 6, 'bola': 20})
+    def test_case_funcao_2_chave_int(self):
+        print('Teste Função 2 item existente')
+        self.assertEqual(mq.funcao2(10,10,{'item1': 1, 'item2': 3, 'item3': 6, 'bola': 10}), None)
+    def test_case_funcao_3(self):
+        print('Teste Função 3')
+        self.assertEqual(mq.funcao3('bola',10,{'item1': 1, 'item2': 3, 'item3': 6, 'bola': 10}),{'item1': 1, 'item2': 3, 'item3': 6})
 if __name__ == '__main__':
     unittest.main(verbosity=2)
 
